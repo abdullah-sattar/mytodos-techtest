b92470c9d3a538619a4422a5a7531cc05ad4e8be
Abdullah <abdullah_a_s@hotmail.co.uk>
2021-11-24 16:44:25 +0000

    some JS done

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector(".main__input");
+const inputBtn = document.querySelector(".main__btn")
+const output = document.querySelector("main__output");
+const completed = document.querySelector(".main__completed")
+
+// const tasks = [];
+
+inputBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    completed.style.display = "none";
+    // tasks.push(input.value);
+    // console.log(tasks);
+    output.innerHTML = `<p>${input.value}</p>`
+});
